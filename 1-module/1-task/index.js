c0bee23a046f2da2b35bb104d7433c52b0e338ae
dmitriiFront <dimagalan95@gmail.com>
2020-02-27 18:32:34 +0300

    1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @returns {number}
  */
 function factorial(n) {
-
+  if (n==0 || n==1) {
+      return 1;
+  } else {
+      for (var i=n-1; i>0; i--) {
+      n = n * i;
+  }
+      return n;
+  }
 }
