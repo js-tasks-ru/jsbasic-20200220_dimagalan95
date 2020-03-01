e3941e1c9b12fe3628be6e58b6ccd5b073127c9f
dmitriiFront <dimagalan95@gmail.com>
2020-03-01 19:00:41 +0300

    1-module 5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,16 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
+  let newStr = '';
 
+  if (str.length > maxlength) {
+
+    for(let i=0; i<maxlength-1; i++) {
+      let symbol = str[i];
+      newStr += symbol;
+    }
+    return newStr + "…";
+  } else {
+    return str;
+  }
 }
