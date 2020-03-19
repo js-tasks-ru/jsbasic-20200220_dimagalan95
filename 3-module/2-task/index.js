c9907ba8d3cdce2b7480dc96f3104c9d2848bc34
dmitriiFront <dimagalan95@gmail.com>
2020-03-19 20:44:00 +0300

    3-module 2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,38 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let lol = str.split(',');
+  console.log(lol);
+  let arr = [];
+  let newArr = [];
+  for (let key of lol) {
+    let a = key.split(' ');
+
+    for (let b of a) {
+        arr.push(b);
+    }
+
+  }
+
+  for (let key of arr) {
+    let keyNext = parseFloat(key);
+
+    if (!keyNext) {
+      continue;
+    } else {
+      newArr.push(keyNext);
+    }
+  }
+
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  newArr.sort(compareNumeric);
+
+  let result = {};
+  result.min = newArr[0];
+  result.max = newArr[newArr.length-1];
+  return result;
 }
