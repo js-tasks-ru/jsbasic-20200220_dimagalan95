dcce6781fb52151fe030cf78a4407523f4e8ec0e
dmitriiFront <dimagalan95@gmail.com>
2020-04-01 11:54:44 +0300

    4-module 1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let  ul = document.createElement('ul');
+  document.body.append(ul);
+
+    for (let i of friends) {
+      let li = document.createElement('li');
+      li.innerHTML = i.firstName +' '+ i.lastName;
+      ul.append(li);
+    }
+    return ul;
 }
